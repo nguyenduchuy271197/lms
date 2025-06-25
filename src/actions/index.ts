882bbe901938658132e049eaf692fea8c05ed509
Huy Nguyen <nguyenduchuy27111997@gmail.com>
2025-06-25 15:50:33 +0700

    refactor

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -34,14 +34,8 @@ export * from "./lessons/publish-lesson"
 export * from "./lessons/reorder-lessons"
 
 // File Management Actions
-export * from "./files/upload-video"
 export * from "./files/upload-thumbnail"
 export * from "./files/upload-avatar"
-export * from "./files/delete-video"
-export * from "./files/delete-thumbnail"
-export * from "./files/get-file-url"
-export * from "./files/get-video-metadata"
-export * from "./files/process-video"
 
 // Enrollment Actions
 export * from "./enrollments/get-my-enrollments"
@@ -55,39 +49,20 @@ export * from "./enrollments/check-enrollment"
 // Lesson Progress Actions
 export * from "./lesson-progress/get-my-lesson-progress"
 export * from "./lesson-progress/get-lesson-progress"
-export * from "./lesson-progress/get-lesson-progress-by-lesson"
 export * from "./lesson-progress/update-lesson-progress"
 export * from "./lesson-progress/mark-lesson-complete"
-export * from "./lesson-progress/mark-lesson-incomplete"
 export * from "./lesson-progress/get-course-progress"
-export * from "./lesson-progress/reset-lesson-progress"
 
 // User Management Actions
 export * from "./user-management/get-all-users"
 export * from "./user-management/get-user-by-id"
 export * from "./user-management/update-user-profile"
 export * from "./user-management/delete-user"
-export * from "./user-management/get-user-statistics"
-export * from "./user-management/get-user-enrollments"
-export * from "./user-management/get-user-activity"
 export * from "./user-management/bulk-action"
-export * from "./user-management/export-user-data"
 
-// Admin Course Management Actions
-export * from "./admin/courses/get-all-courses"
-export * from "./admin/courses/get-course-analytics"
-export * from "./admin/courses/bulk-update-courses"
-export * from "./admin/courses/export-courses"
-export * from "./admin/courses/get-course-engagement"
-export * from "./admin/courses/get-popular-courses"
-export * from "./admin/courses/get-completion-rates"
+
 
 // Admin Student Management Actions
 export * from "./admin/students/get-all-students"
 export * from "./admin/students/get-student-details"
 export * from "./admin/students/get-student-progress"
-export * from "./admin/students/get-student-enrollments"
-export * from "./admin/students/get-student-analytics"
-export * from "./admin/students/export-student-data"
-export * from "./admin/students/get-learning-path"
-export * from "./admin/students/reset-student-progress"
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -34,14 +34,8 @@ export * from "./lessons/use-publish-lesson"
 export * from "./lessons/use-reorder-lessons"
 
 // File Management Hooks
-export * from "./files/use-upload-video"
 export * from "./files/use-upload-thumbnail"
 export * from "./files/use-upload-avatar"
-export * from "./files/use-delete-video"
-export * from "./files/use-delete-thumbnail"
-export * from "./files/use-file-url"
-export * from "./files/use-video-metadata"
-export * from "./files/use-process-video"
 
 // Enrollment Hooks
 export * from "./enrollments/use-my-enrollments"
@@ -55,42 +49,23 @@ export * from "./enrollments/use-check-enrollment"
 // Lesson Progress Hooks
 export * from "./lesson-progress/use-my-lesson-progress"
 export * from "./lesson-progress/use-lesson-progress"
-export * from "./lesson-progress/use-lesson-progress-by-lesson"
 export * from "./lesson-progress/use-update-lesson-progress"
 export * from "./lesson-progress/use-mark-lesson-complete"
-export * from "./lesson-progress/use-mark-lesson-incomplete"
 export * from "./lesson-progress/use-course-progress"
-export * from "./lesson-progress/use-reset-lesson-progress"
 
 // User Management Hooks
 export * from "./user-management/use-all-users"
 export * from "./user-management/use-user-by-id"
 export * from "./user-management/use-update-user-profile"
 export * from "./user-management/use-delete-user"
-export * from "./user-management/use-user-statistics"
-export * from "./user-management/use-user-enrollments"
-export * from "./user-management/use-user-activity"
 export * from "./user-management/use-bulk-action"
-export * from "./user-management/use-export-user-data"
 
-// Admin Course Management Hooks
-export * from "./admin/courses/use-all-courses"
-export * from "./admin/courses/use-course-analytics"
-export * from "./admin/courses/use-bulk-update-courses"
-export * from "./admin/courses/use-export-courses"
-export * from "./admin/courses/use-course-engagement"
-export * from "./admin/courses/use-popular-courses"
-export * from "./admin/courses/use-course-completion"
+
 
 // Admin Student Management Hooks
 export * from "./admin/students/use-all-students"
 export * from "./admin/students/use-student-details"
 export * from "./admin/students/use-student-progress"
-export * from "./admin/students/use-student-enrollments"
-export * from "./admin/students/use-student-analytics"
-export * from "./admin/students/use-export-student-data"
-export * from "./admin/students/use-learning-path"
-export * from "./admin/students/use-reset-student-progress"
 
 // Utility Hooks
 export * from "./use-mobile"
